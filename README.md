85908b09cdad100bdabcb86f954e56ae37bc0b40
Yiming Pang <ympang@trip.com>
2021-11-25 10:29:00 +0800

    update Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-10-20 09:37:25
+ * @LastEditTime: 2021-11-25 10:27:41
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /Web/README.md
+-->
 "# Web" 
+[检测网页中指定字符串，并提示](./检测网页中指定字符串，并提示.ts)
+[用当前目录下的文件生成思维导图源文件](./fileMap.ts)
+[使用antd树的结构，生成思维导图源文件](./treeMap.ts)
+
--- treeMap.ts
+++ treeMap.ts
@@ -1,80 +1,91 @@
-"use strict";
-exports.__esModule = true;
-exports.getItemFromArrByKeyValue = void 0;
-var fs = require('fs');
-var path = require('path');
-var uuid = require('uuid');
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
 // console.log(uuid.v4())
 // sync version
 function walkSync(currentDirPath, callback) {
+
     fs.readdirSync(currentDirPath).forEach(function (name) {
         var filePath = path.join(currentDirPath, name);
         var stat = fs.statSync(filePath);
         if (stat.isFile()) {
             callback(filePath, stat);
-        }
-        else if (stat.isDirectory()) {
+        } else if (stat.isDirectory()) {
             callback(filePath, stat);
             walkSync(filePath, callback);
         }
     });
 }
-var dirPath = '/Users/lyndonpang/workspace/Web';
-var rootName = 'Web';
-var rootNodes = {
+const dirPath = '/Users/lyndonpang/workspace/Web'
+const rootName = 'Web'
+const rootNodes = {
     "id": "rmind_root_node", "text": rootName, "showChildren": true, "children": []
-};
-var root = rootNodes;
+}
+let root: Element = rootNodes
 walkSync(dirPath, function (filePath, stat) {
     // do something with "filePath"...
     if (filePath.indexOf(dirPath + '/.git') === -1) {
-        var relative = filePath.replace(dirPath + '/', '');
-        var pathArr = relative.split('/');
+        var relative = filePath.replace(dirPath + '/', '')
+        const pathArr: string[] = relative.split('/')
         // console.log('path', pathArr)
-        var node = {
+
+        const node = {
             "id": uuid.v4(), "text": pathArr[pathArr.length - 1],
             "showChildren": true,
             "children": [],
             filePath: relative,
             isDir: stat.isDirectory()
-        };
+        }
         // TODO 找到对应的节点
         if (pathArr.length !== 1) {
             // console.log('path', pathArr)
-            root = getByKeyValue(rootNodes, pathArr.slice(0, -1), 'text');
+            root = getByKeyValue(rootNodes, pathArr.slice(0, -1), 'text')
+        } else {
+            root = rootNodes
         }
-        else {
-            root = rootNodes;
-        }
-        root.children.push(node);
+        root.children.push(node)
+
+
     }
+
 });
 console.log(JSON.stringify(rootNodes));
-fs.writeFileSync(dirPath + '/目录大纲.rmf', JSON.stringify(rootNodes));
-function getByKeyValue(root, path, key) {
-    var node = root;
-    for (var i = 0; i < path.length; i++) {
-        var p = path[i];
-        // TODO 根据 text 找到数组
-        node = getItemFromArrByKeyValue(node.children, key, p);
-    }
-    return node;
+
+fs.writeFileSync(dirPath+'/目录大纲.rmf',JSON.stringify(rootNodes))
+
+interface Element {
+    children: Element[]
+    [key: string]: unknown
 }
+
+function getByKeyValue(root: Element, path: string[], key: string): Element {
+    let node = root
+
+    for (let i = 0; i < path.length; i++) {
+        const p = path[i]
+        // TODO 根据 text 找到数组
+        node = getItemFromArrByKeyValue(node.children, key, p)
+    }
+
+    return node
+}
+
 /**
  * 跟数组中item的key和value拿到对应的item
  * @param array 源数据数组
  * @param key 要找的key
  * @param value 要找的value，源数据数组中item的key的value和他相等时候会返回
- * @returns
+ * @returns 
  */
-function getItemFromArrByKeyValue(array, key, value) {
-    for (var i = 0; i < array.length; i++) {
-        var item = array[i];
+export function getItemFromArrByKeyValue(array, key, value) {
+    for (let i = 0; i < array.length; i++) {
+        const item = array[i];
         if (item && item[key].toString() === value.toString()) {
-            return item;
+            return item
         }
     }
-    console.error("Cannot find a descendant at path [" + value + "] in node: " + JSON.stringify(array));
-    return;
+    console.error(
+        `Cannot find a descendant at path [${value}] in node: ${JSON.stringify(array)}`
+    )
+    return
 }
-exports.getItemFromArrByKeyValue = getItemFromArrByKeyValue;
